b087cbe1eda872487fff29feda9fdae253c449ee
허건 <braveman0823@gmail.com>
2024-07-12 20:12:12 +0900

    jsx기본과 세팅 관련 커밋

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//root의 첫번째 렌더링 되는 컴포넌트는 App인걸 알 수 있다. 그리고 StrictMode를 통해서 엄격한 규칙을 적용받음
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
