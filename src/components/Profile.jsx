83d2179dc5e37994f13462d9576ad6321fd5ee80
허건 <braveman0823@gmail.com>
2024-07-18 23:37:55 +0900

    props쓰는방법이랑 간단한 프로필 만들기

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,3 +1,12 @@
-export default function Profile() {
-  return <h1>Profile</h1>;
+import React from "react";
+//props로 쓰면 일일이 props.name 이런식으로 써야 하는데 props에 {image, name, title} 이렇게 써주면 굳이 props 안써줘도 됨
+export default function Profile({ image, name, title, isNew }) {
+  return (
+    <div className="profile">
+      <img className="photo" src={image} alt="photo image" />
+      {isNew && <span className="new">New</span>}
+      <h1>{name}</h1>
+      <p>{title}</p>
+    </div>
+  );
 }
