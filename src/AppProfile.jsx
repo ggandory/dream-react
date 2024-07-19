c217b62cf18d6fd384f0b1e9af70f09335bbe292
허건 <braveman0823@gmail.com>
2024-07-19 19:45:42 +0900

    avatar만들기

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Avatar from "./components/Avatar";
 import Profile from "./components/Profile";
 //html이랑 비슷하게 쓸 수 있다 그러나 js 파일이라서 자바스크립트도 쓸 수 있음
 //하나의 컴포넌트는 꼭 하나의 태그만 반환해야함 만약 여러개의 태그를 반환하고 싶으면 부모태그를 만들어야 한다. <div> <> 이런식으로 감싸줘야함
@@ -7,6 +8,11 @@ import Profile from "./components/Profile";
 function AppProfile() {
   return (
     <>
+      <Avatar
+        image="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+        isNew="true"
+      />
+
       <Profile
         image="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
         name="Heo Geon"
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import Avatar from "./Avatar";
 //props로 쓰면 일일이 props.name 이런식으로 써야 하는데 props에 {image, name, title} 이렇게 써주면 굳이 props 안써줘도 됨
 export default function Profile({ image, name, title, isNew }) {
   return (
     <div className="profile">
-      <img className="photo" src={image} alt="photo image" />
-      {isNew && <span className="new">New</span>}
+      <Avatar image={image} isNew={isNew} />
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
