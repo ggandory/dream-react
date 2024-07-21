b7e25d7d47b03c48c9bc2446badb9645ca7201d6
허건 <braveman0823@gmail.com>
2024-07-21 23:48:44 +0900

    이벤트 처리하기 예시

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -6,8 +6,16 @@ import Profile from "./components/Profile";
 //class 대신에 className으로 표현해야함
 //자바스크립트 코드를 html로 표현하려면 중괄호로 묶어줘야함
 function AppProfile() {
+  const handleClick = (event) => {
+    console.log(event);
+    alert("버튼이 클릭됨!");
+  };
   return (
     <>
+      <button onClick={handleClick}>버튼</button>
+      <form onSubmit={() => {}}>
+        <input />
+      </form>
       <Avatar
         image="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
         isNew="true"
